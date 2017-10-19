4243c5f23822c9d614fdf7a14d50c1ec448b67ef
vedant05 <vedantkumar05@gmail.com>
2017-10-19 19:21:48 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE
+devenv "C:\ZiplockDotnetFramework\SWAUTCSharpFramework.sln"/build
+cd C:\ZiplockDotnetFramework\bin\Debug\
+SeleniumFirst.exe
