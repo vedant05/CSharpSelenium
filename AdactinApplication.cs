2e3c29a267669db24719689a64f396f1a4506ddf
vedant05 <vedantkumar05@gmail.com>
2017-10-19 19:41:40 +0530

    Add files via upload

---
+++ AdactinApplication.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Threading;
+using NUnit.Framework;
+using SWAUTCSharpFramework.Utilities;
+
+namespace SWAUTCSharpFramework
+{
+
+    public class AdactinApplication
+    {
+      
+        private String url;
+        private String env;
+        private Page page;
+
+        public static IWebDriver browser { get; set; }
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+      
+        public AdactinApplication()
+        {
+            String strbrowser = Common.GetConfigProperty("Browser");
+            browser = Common.getdriver(strbrowser);
+            Common.browser = browser;
+            this.url = Common.GetConfigProperty("url");
+          
+        }
+
+
+
+        public AdactinHomePage openAdactinApplication()
+        {
+            try
+            {
+                Common.browser = browser;
+                browser.Navigate().GoToUrl(Common.GetConfigProperty("url"));
+                browser.Manage().Window.Maximize();
+               			
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return new AdactinHomePage(browser);
+
+        }
+
+        //=================================================================
+
+        /**
+         * method to make a thread sleep for customized time in milliseconds
+         * @param milliseconds
+         */
+        protected void sleep(int milliseconds)
+        {
+            try
+            {
+                Thread.Sleep(milliseconds);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+            void deleteAllCookies()
+            {
+                try
+                {
+                browser.Manage().Cookies.DeleteAllCookies();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+
+            /**
+             * Get relative path of the framework
+             * @return
+             */
+            public string getRelativePath()
+            {
+                string relativePath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+                return relativePath;
+            }
+
+            public void close()
+            {
+                try {
+                deleteAllCookies();
+                browser.Quit();
+                    if (GenericKeywords.testFailure)
+                    {
+                        Assert.Fail("Testcasefailed");
+                    }
+                } catch (Exception Ex)
+                {
+                    Console.WriteLine("Unable to Close Application" + Ex);
+                }
+            }
+
+        //public static string TASKLIST = "tasklist";
+        //public static string KILL = "taskkill /IM ";
+
+        //public static void IsProcessRunging(String serviceName)
+        //{
+
+
+        //    foreach (var process in Process.GetProcessesByName("whatever"))
+        //    {
+        //        process.Kill();
+        //    }
+
+        //    //          Process p = Runtime.getRuntime().exec(TASKLIST);
+        //    //              BufferedReader reader = new BufferedReader(new InputStreamReader(
+        //    //                      p.getInputStream()));
+        //    //	String line;
+        //    //	while ((line = reader.readLine()) != null) {
+        //    //		if (line.Contains(serviceName)) {
+        //    //			return true;
+        //    //		}
+        //    //	}
+
+        //    //	return false;
+
+        //    //}
+
+        //    public static void KillProcess(String serviceName)
+        //    {
+
+        //        Runtime.getRuntime().exec(KILL + serviceName);
+
+        //    }
+        }
+    }
---
+++ SelectHotel.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAUTCSharpFramework
+{
+    public class SelectHotel : Page
+    {
+        protected static String HOME_PAGE_TITLE = "AdactIn.com - Select Hotel";
+        public SelectHotel(IWebDriver browser) : base(browser)
+        {
+            //other stuff here
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='radiobutton_0']")]
+        public IWebElement rbSelect { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='continue']")]
+        public IWebElement btnContinue { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='cancel']")]
+        public IWebElement btnCancel { get; set; }
+
+
+
+
+        protected override bool isValidPage()
+        {
+            try
+            {
+                if (browser.Title.Trim().Contains(HOME_PAGE_TITLE))
+                {
+
+                    return true;
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+            return false;
+        }
+        protected override void waitForPageLoad()
+        {
+            try
+            {
+                new WebDriverWait(browser, TimeSpan.FromSeconds(50)).Until(ExpectedConditions.ElementExists((By.XPath("//input[@id='cancel']"))));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+        }
+
+
+        public void Selecthotel()
+        {
+            clickOn(rbSelect, "Selected");
+           // Common.testStepPassed("Selected radio button");
+            clickOn(btnContinue, "Continue");
+           // Common.testStepPassed("Clicked on Continue Button");
+        }
+
+    }
+}
